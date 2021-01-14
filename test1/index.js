15bf79a3a8a7c09bd23f888b6cbc931c88a35d1b
gilwohl <gil.wohl@gmail.com>
2021-01-14 09:56:46 -0800

    basic numbers

--- test1/index.js
+++ test1/index.js
@@ -1,3 +1,5 @@
+///////////////////////// boolean logic
+
 TTT = x => y => x;
 FFF = x => y => y;
 
@@ -8,40 +10,67 @@ eq = x => y => x(y)(not(y));
 xor = x => y => not(eq(x)(y));
 
 
+///////////////////////// boolean logic demo
 
-var bools = [FFF,TTT];
+var BOOLS = [FFF,TTT];
 bool2Str = b => b("T")("F");
 printBool = b => console.log(bool2Str(b));
 
-bools.forEach(x => { printBool(x); });
+BOOLS.forEach(x => { printBool(x); });
 console.log("---");
 
-
-bools.forEach(x => { console.log("NOT " + bool2Str(x) + " : " + bool2Str(not(x))) });
-
+BOOLS.forEach(x => { console.log("NOT " + bool2Str(x) + " : " + bool2Str(not(x))) });
 console.log("---");   
 
-bools.forEach(x => { bools.forEach(y => { 
-            console.log(bool2Str(x) + " OR " + bool2Str(y) + " : " + bool2Str(or(x)(y)));
-    }) });
-
+BOOLS.forEach(x => { BOOLS.forEach(y => { 
+            console.log(bool2Str(x) + " OR " + bool2Str(y) + " : " + bool2Str(or(x)(y)));  }) });
 console.log("---");    
 
-bools.forEach(x => 
-    { bools.forEach(y => { 
-            console.log(bool2Str(x) + " AND " + bool2Str(y) + " : " + bool2Str(and(x)(y)));
-    }) });
-    
+BOOLS.forEach(x => 
+    { BOOLS.forEach(y => { 
+            console.log(bool2Str(x) + " AND " + bool2Str(y) + " : " + bool2Str(and(x)(y)));  }) });
 console.log("---");    
 
-bools.forEach(x => 
-    { bools.forEach(y => { 
-            console.log(bool2Str(x) + " == " + bool2Str(y) + " : " + bool2Str(eq(x)(y)));
-    }) });
-    
+BOOLS.forEach(x => 
+    { BOOLS.forEach(y => { 
+            console.log(bool2Str(x) + " == " + bool2Str(y) + " : " + bool2Str(eq(x)(y)));  }) });
 console.log("---");    
 
-bools.forEach(x => 
-    { bools.forEach(y => { 
-            console.log(bool2Str(x) + " XOR " + bool2Str(y) + " : " + bool2Str(xor(x)(y)));
-    }) });    
+BOOLS.forEach(x => 
+    { BOOLS.forEach(y => { 
+            console.log(bool2Str(x) + " XOR " + bool2Str(y) + " : " + bool2Str(xor(x)(y)));  }) });    
+console.log("---");  
+
+
+///////////////////////// numbers
+
+ZERO = f => x => x;
+//ONE = f => x => f(x);
+//TWO = f => x => f(f(x));
+succ = n => f => x => f(n(f)(x))
+ONE = succ(ZERO);
+TWO = succ(ONE);
+THREE = succ(TWO);
+FOUR = succ(THREE);
+
+NUMBERS = [ZERO];
+for (i=0; i<10; ++i)
+{
+    let last = NUMBERS[NUMBERS.length-1];
+    NUMBERS.push(succ(last));
+}
+
+///////////////////////// numbers demo
+
+num2Str = n => n (x=>x+1)(0);
+printNum = n => console.log(num2Str(n));
+
+printNum(ZERO);
+printNum(ONE);
+printNum(TWO);
+printNum(THREE);
+console.log("---");  
+let numbersStr = "";
+NUMBERS.forEach(n => numbersStr += (num2Str(n) + ", "));
+console.log(numbersStr);
+console.log("---");  
